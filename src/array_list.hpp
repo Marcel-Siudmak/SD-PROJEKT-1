9aff45ab302596ac9d2013b6d04dce51ff4727eb
Mazo-chan <jaklac1105@gmail.com>
2026-03-21 12:28:05 +0100

    array management methods: add, remove, resize. template
    https://youtu.be/QM84uM7c2Og

--- src/array_list.hpp
+++ src/array_list.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include "IList.hpp"
 
-class array_list : public IList<int> {
+template <class T> 
+class array_list : public IList<T> {
     private:
-        int* _arr;
+        T* _arr;
         int _size;
         int _capacity;
+        void resize(int new_capacity){};
     public:
         array_list(){};
         array_list(int cap){};
@@ -15,15 +17,15 @@ class array_list : public IList<int> {
 
         array_list& operator=(const array_list& other){};
         
-        void push_front(int value) override = 0;
-        void push_back(int value) override = 0;
-        void insert(int value, int index) override = 0;
+        void push_front(T value) override = 0;
+        void push_back(T value) override = 0;
+        void insert(T value, int index) override = 0;
 
         void pop_front() override = 0;
         void pop_back() override = 0;
         void remove(int index) override = 0;
 
-        bool find(int value) override = 0;
+        bool find(T value) override = 0;
 
         void display() override = 0;
         void clear() override = 0;
